7f6d2e093de1ed74c5e624dc505e847c99999ecf
Travis Lamberte <travis@lamberte.com>
2023-11-20 15:14:02 -0800

    add test case

---
+++ leetcode-1323/leet-1323.py
@@ -0,0 +1,11 @@
+def maximum69Number (num):
+    place = 10000
+    while place > 0:
+        digit = (num - num % place) / place
+        if digit % 10 == 6:
+            result = int(num + (place * 3))
+            return result
+        place = place / 10
+    return num
+
+print(maximum69Number(9669))
