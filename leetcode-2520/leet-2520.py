e343afc2a9b43cd137fbf278eaebd4329b38a24a
Travis M Lamberte <129416424+seattletravis@users.noreply.github.com>
2023-11-23 08:15:39 -0800

    Update leet-2520.py

--- leetcode-2520/leet-2520.py
+++ leetcode-2520/leet-2520.py
@@ -1,14 +1,13 @@
 import math
 
 def countDigits(num):
-  num = 1248
   instance = num
   count = 0
 
   while instance > 0:
-    divisor = instance % 10
-    instance = math.floor(instance / 10)
-    if num % divisor == 0:
+    digit = instance % 10
+    instance = instance // 10
+    if num % digit == 0:
       count += 1
   return count
 
