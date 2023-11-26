06a13914c41b3cad0de254c8af78bc0c0c80846d
Travis M Lamberte <129416424+seattletravis@users.noreply.github.com>
2023-11-25 17:40:48 -0800

    add test cases

--- leetcode-70/leet-70.py
+++ leetcode-70/leet-70.py
@@ -1,26 +1,30 @@
-class Solution(object):
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        2:2
-        3:3
-        4:5
-        5:8
-        """
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        pointer = 3
-        a = 1
-        b = 2
 
-        while pointer < n + 1:
-            temp = a
-            a = b
-            b = temp + b
-            pointer += 1
-        return b 
+def climbStairs(n):
+    """
+    :type n: int
+    :rtype: int
+    2:2
+    3:3
+    4:5
+    5:8
+    """
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    pointer = 3
+    a = 1
+    b = 2
+
+    while pointer < n + 1:
+        temp = a
+        a = b
+        b = temp + b
+        pointer += 1
+    return b 
+
+print(climbStairs(2))
+print(climbStairs(3))
+print(climbStairs(4))
