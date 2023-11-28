0f3ab82f94030692bc10b5921d2bad798fbf4e88
Travis M Lamberte <129416424+seattletravis@users.noreply.github.com>
2023-11-28 11:17:54 -0800

    Update leet-125.py

--- leetcode-125/leet-125.py
+++ leetcode-125/leet-125.py
@@ -1,1 +1,15 @@
+#Intuitive Way to solve this
+
+def isPalindrome(s):
+    stripped = ''
+    deppirts = '' 
+    for letter in s:
+        if letter.isalnum():
+            stripped += letter.lower()
+            deppirts = letter.lower() + deppirts
+    print(stripped, deppirts)
+    return stripped == deppirts
+
+#slightly better:
+
 
