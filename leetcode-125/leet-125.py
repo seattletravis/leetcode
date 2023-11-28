6506bc4130423b8da801507fbfed0c2043d2878c
Travis M Lamberte <129416424+seattletravis@users.noreply.github.com>
2023-11-28 11:20:13 -0800

    Update leet-125.py

--- leetcode-125/leet-125.py
+++ leetcode-125/leet-125.py
@@ -11,5 +11,16 @@ def isPalindrome(s):
     return stripped == deppirts
 
 #slightly better:
+#Remove string to string comparison and compared string to reverse iteration over string
+def isPalindrome(s):
+    stripped = ''
+    for letter in s:
+        if letter.isalnum():
+            stripped += letter.lower()
+    return stripped == stripped[::-1] # <--
+
+#slightly better still
+
+
 
 
